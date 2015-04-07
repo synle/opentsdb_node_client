58854753421fabf8ff8571876bce16974968ebe1
Sy Le <lenguyensy@gmail.com>
2015-04-06 19:21:55 -0700

    added more tests

--- test/integrationTest.js
+++ test/integrationTest.js
@@ -1,5 +1,5 @@
 //public
-var chai = require("chai")
+var chai = require("chai");
 var assert = chai.assert;
 
 //internal
@@ -10,8 +10,64 @@ var myTsdbClient = new TsdbClient({
 });
 
 describe('TsdbClient', function() {
-    describe('#getAggregators', function() {
-        it('TsdbClient.getAggregators', function(done) {
+    describe('TsdbClient.suggest', function() {
+        it('TsdbClient.suggestMetrics should work', function(done) {
+            myTsdbClient.suggestMetrics(
+                'sys',
+                3
+            ).then(function(r) {
+                assert.equal(
+                    r.length > 0 && r.length <= 3,
+                    true
+                );
+
+                assert.equal(
+                    r[0].indexOf('sys') >= 0,
+                    true
+                );
+
+                done();
+            });
+        });
+
+        it('TsdbClient.suggestTagK should work', function(done) {
+            myTsdbClient.suggestTagK(
+                'host',
+                3
+            ).then(function(r) {
+                assert.equal(
+                    r.length > 0 && r.length <= 3,
+                    true
+                );
+
+                assert.equal(
+                    r[0].indexOf('host') >= 0,
+                    true
+                );
+
+                done();
+            });
+        });
+
+        it('TsdbClient.suggestTagV should work', function(done) {
+            myTsdbClient.suggestTagV(
+                '',
+                3
+            ).then(function(r) {
+                assert.equal(
+                    r.length > 0 && r.length <= 3,
+                    true
+                );
+
+
+                done();
+            });
+        });
+    });
+
+
+    describe('misc', function() {
+        it('TsdbClient.getAggregators should work', function(done) {
             myTsdbClient.getAggregators().then(function(r) {
                 var expectedAggregators = [
                     'min',
@@ -22,7 +78,7 @@ describe('TsdbClient', function() {
                     'sum',
                     'avg',
                     'zimsum'
-                ]
+                ];
 
                 //run through and delete the list
                 assert.equal(r.length > 0, true);
@@ -30,11 +86,51 @@ describe('TsdbClient', function() {
                     assert.equal(
                         expectedAggregators.indexOf(agg) >= 0,
                         true
-                    )
+                    );
                 });
 
                 done();
             });
-        })
-    })
-})
+        });
+
+
+        it('TsdbClient.version should work', function(done) {
+            myTsdbClient.version().then(function(r) {
+
+                assert.isDefined(r.timestamp);
+                assert.isDefined(r.host);
+                assert.isDefined(r.repo);
+                assert.isDefined(r.full_revision);
+                assert.isDefined(r.short_revision);
+                assert.isDefined(r.user);
+                assert.isDefined(r.repo_status);
+                assert.isDefined(r.version);
+
+                done();
+            });
+        });
+
+        it('TsdbClient.serializers should work', function(done) {
+            myTsdbClient.serializers().then(function(r) {
+                assert.equal(
+                    r.length > 0,
+                    true
+                )
+
+                done();
+            });
+        });
+
+
+        it('TsdbClient.stats should work', function(done) {
+            myTsdbClient.stats().then(function(r) {
+                assert.equal(
+                    r.length > 0,
+                    true
+                )
+
+                done();
+            });
+        });
+    });
+});
